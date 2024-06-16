d0b8d5c64409388536ab1875ce93f44c919256f5
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-06-16 16:03:51 -0300

    Create project

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class Main {
+    //Programa que puxa e exibe a sua
+    public static void main(String[] args) {
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        System.out.println("A resolução da sua máquina é: "+d.width+ "x" +d.height);
+    }
+}
