462fdb57372c343b6faf9bebadbd3b6cfff87fa0
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-06-21 00:07:56 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 
 public class Main {
-    //Programa que puxa e exibe a resolução da sua tel!!!
+    //Programa que puxa e exibe a resolução da sua tel!
     public static void main(String[] args) {
         Toolkit tk = Toolkit.getDefaultToolkit();
         Dimension d = tk.getScreenSize();
